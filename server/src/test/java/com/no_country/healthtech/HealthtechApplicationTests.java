e1aaed4fc4a92b08733370e2ff87a0aa159f49ba
rudetm32 <jaruiz721818@gmail.com>
2024-08-26 01:29:27 -0600

    first commit

---
+++ server/src/test/java/com/no_country/healthtech/HealthtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.no_country.healthtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
