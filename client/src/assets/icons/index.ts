62c7cccfdeecc49c0bcc12365350eb024af9b337
Valentín Vera <valentinvera2805@gmail.com>
2024-09-01 21:02:55 -0300

    feat: Add folder assets with images and folder icons

---
+++ client/src/assets/icons/index.ts
@@ -0,0 +1,35 @@
+import DentistryIcon from "./dentistry.svg"
+import CardiologyIcon from "./cardiology.svg"
+import PulmonologyIcon from "./pulmonology.svg"
+import GeneralIcon from "./general.svg"
+import NeurologyIcon from "./neurology.svg"
+import GastroenterologyIcon from "./gastroenterology.svg"
+import LaboratoryIcon from "./laboratory.svg"
+import VaccinationIcon from "./vaccination.svg"
+import HomeIcon from "./home.svg"
+import LocationIcon from "./location.svg"
+import CalendarIcon from "./calendar.svg"
+import UserIcon from "./user.svg"
+import SearchIcon from "./search.svg"
+import LocationIconDark from "./locationdark.svg"
+import BellIcon from "./bell.svg"
+import DotIcon from "./dot.svg"
+
+export {
+  DentistryIcon,
+  CardiologyIcon,
+  PulmonologyIcon,
+  GeneralIcon,
+  NeurologyIcon,
+  GastroenterologyIcon,
+  LaboratoryIcon,
+  VaccinationIcon,
+  HomeIcon,
+  LocationIcon,
+  CalendarIcon,
+  UserIcon,
+  SearchIcon,
+  LocationIconDark,
+  BellIcon,
+  DotIcon,
+}
