556732be970f2285c9845c3d75fc4ee92f7de5b3
Valentín Vera <valentinvera2805@gmail.com>
2024-09-06 19:32:39 -0300

    fix: Rename values of the state and set state again

--- client/src/components/SearchInput.tsx
+++ client/src/components/SearchInput.tsx
@@ -6,30 +6,30 @@ import Modal from "@/components/Modal"
 interface ExtendedFormEvent extends ChangeEvent<HTMLInputElement> {}
 
 const SearchInput = () => {
-  const [surname, setSurname] = useState<string>("")
-  const [data, loading, error] = useFetchDoctor(surname)
+  const [apellido, setApellido] = useState<string>("")
+  const [data, loading, error] = useFetchDoctor(apellido)
   const [showModal, setShowModal] = useState<boolean>(true)
 
   const handleInputChange = (e: ExtendedFormEvent) => {
-    setSurname(e.target.value)
+    setApellido(e.target.value)
     setShowModal(true)
   }
 
   return (
-    <div className="mb-2 relative">
-      <figure className="absolute top-2 pl-4 ">
+    <div className="relative mb-2">
+      <figure className="absolute top-2 pl-4">
         <img src={SearchIcon} alt="" />
       </figure>
       <input
         type="text"
         placeholder="Search doctor..."
-        className="mb-2 h-[40px] outline-none w-full rounded-xl bg-[#f3f4f6] pl-[52px]"
-        value={surname}
+        className="mb-2 h-[40px] w-full rounded-xl bg-[#f3f4f6] pl-[52px] outline-none"
+        value={apellido}
         onChange={handleInputChange}
       />
-      {data.length > 0 && showModal && data.map((doctor) => (
-        <Modal key={doctor.id} doctor={doctor} />
-      ))}
+      {data.length > 0 &&
+        showModal &&
+        data.map(doctor => <Modal key={doctor.idMedico} doctor={doctor} />)}
       {loading && <p>Cargando...</p>}
       {error instanceof Error && <p>Error: {error.message}</p>}
     </div>
